d1878e67d002cb969c1843ec421ea7042ad896a2
shikher.gupta <shikher.gupta@extramarks.net>
2021-01-27 14:55:27 +0530

    Minor change

--- src/main/java/utils/TestBase.java
+++ src/main/java/utils/TestBase.java
@@ -2,12 +2,12 @@ package utils;
 
 import com.extramarks.foundation.core.P;
 import com.google.common.collect.ImmutableMap;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
 import static com.extramarks.foundation.customreporter.AllureEnvironmentVariableSetUp.allureEnvironmentVariableSetUp;
 
 public class TestBase {
 
-    @BeforeSuite
+    @AfterSuite
     public void setAllureEnvironment() {
         allureEnvironmentVariableSetUp(
                 ImmutableMap.<String, String>builder()
